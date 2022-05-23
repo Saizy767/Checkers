4ccc36c1b7d46fdbcb8129f274777fff029326fc
Saizy767 <kuklin230101@mail.ru>
2022-05-23 10:41:15 +0300

    adaptive on scss

--- components/Cell/CellComponent.tsx
+++ components/Cell/CellComponent.tsx
@@ -12,17 +12,19 @@ const CellComponent: FC<CellProps>=({cell, selected, handleCellClick})=>{
     return(
         <div className={`${styles.cell} 
                         ${cell.color === "white" ? styles.cell_white : styles.cell_black}
-                        ${selected && cell.figure ? styles.selected : ''}`}
+                        ${selected && cell.figure ? styles.selected : ''}
+                        ${cell.figure? styles.pointer: ''}`}
                         onClick={()=>handleCellClick(cell)} 
                         style={{cursor: cell.available 
                                 && !cell.figure?.form 
                                 ? 'pointer': '',
-                                backgroundColor: cell.available && cell.figure?.form ? 'greenyellow' : ''}}>
+                                backgroundColor: cell.available && cell.figure?.form ? 'greenyellow' : '',
+                                }}>
             {cell.available && 
             !cell.figure?.form &&
               <div className={styles.cell_available}/>}
             {cell.figure?.form && 
-                <Image src={cell.figure.form} alt={'cell.figure.name'} className={styles.cell__figure}/>}
+                <Image src={cell.figure.form} alt={(cell.figure.id).toString()}/>}
         </div>
     )
 }
